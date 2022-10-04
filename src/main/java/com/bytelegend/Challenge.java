fcb1d3d7600a600814089856190c4145455d70b0
QianChiLang1 <1149956184@qq.com>
2022-10-04 03:22:43 +0000

    Update at 2022-10-04T03:22:43.356918Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,10 +2,32 @@ package com.bytelegend;
 
 public class Challenge {
     public static int add(int a, int b) {
-        return a - b;
+        return a + b;
     }
 
     public static void main(String[] args) {
         System.out.println("1+1=" + add(1, 1));
     }
 }
+[INFO] 
+[INFO] -------------------------------------------------------
+[INFO]  T E S T S
+[INFO] -------------------------------------------------------
+[INFO] Running com.bytelegend.ChallengeTest
+[INFO] Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.019 s - in com.bytelegend.ChallengeTest
+[INFO] 
+[INFO] Results:
+[INFO] 
+[INFO] Tests run: 1, Failures: 0, Errors: 0, Skipped: 0
+[INFO] 
+[INFO] 
+[INFO] --- maven-jar-plugin:2.4:jar (default-jar) @ java-fix-add ---
+[INFO] Building jar: C:\Users\千尺浪\java-fix-add\target\java-fix-add-1.0-SNAPSHOT.jar
+[INFO] ------------------------------------------------------------------------
+[INFO] BUILD SUCCESS
+[INFO] ------------------------------------------------------------------------
+[INFO] Total time:  6.520 s
+[INFO] Finished at: 2022-10-04T11:21:30+08:00
+[INFO] ------------------------------------------------------------------------
+
+进程已结束，退出代码为 0
